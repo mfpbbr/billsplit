31645f87479c7159339dd09218614e5d3f0ff1ff
Belda Chan <beldabelda@gmail.com>
2013-07-04 09:58:36 -0700

    Validations for submitting bill form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render :json => @user.to_json }
+    end
   end
 
   def new
