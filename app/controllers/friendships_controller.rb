1d432fbdfd945cb852fa41de7eb30a737f4cc922
Belda Chan <beldabelda@gmail.com>
2013-07-05 20:03:46 -0700

    "Fixed validation for adding friends."

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,13 +1,19 @@
 class FriendshipsController < ApplicationController  
   def create
     friend = User.find_by_email(params[:friendship][:friend_email])
-
-    friendship = Friendship.new(:user_id => current_user.id, :friend_id => friend.id) if friend
+    does_not_exist = Friendship.where(:user_id => current_user.id, :friend_id => friend.id).empty? && Friendship.where(:user_id => friend.id, :friend_id => current_user.id).empty?
     
-    if friend && friendship.save
-      flash[:notice] = "Friendship successfully created."
+    if (!does_not_exist)
+      flash[:notice] = "You are already friends."
     else
-      flash[:notice] = "No user with that email, or you are already friends."
+
+      friendship = Friendship.new(:user_id => current_user.id, :friend_id => friend.id) if friend
+    
+      if friend && friendship.save
+        flash[:notice] = "Friendship successfully created."
+      else
+        flash[:notice] = "No user with that email, or you are already friends."
+      end
     end
     
     redirect_to root_url
